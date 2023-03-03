b34dc2b2a99ead06294617425e9fda1f9327fa8c
SUDHARSANAN S R <109327528+sudharsanan123@users.noreply.github.com>
2023-03-03 21:23:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var weight, height, measure, bmi, error ;
-
 function calculate() {
 	weight = document.getElementById("weight").value;
 	height = document.getElementById("height").value;
@@ -10,13 +9,13 @@ function calculate() {
 	bmi = bmi.toFixed(1);
 
 	if (bmi <= 18.4) {
-		measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
+		measure = "Your BMI is " + bmi + " which means " + "you are Underweight increase your weight ";
 	} else if (bmi >= 18.5 && bmi <= 24.9) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Normal";
+		measure = "Your BMI is " + bmi + " which means " + "You are Normal and healthy";
 	} else if (bmi >= 25 && bmi <= 29.9) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
+		measure = "Your BMI is " + bmi + " which means " + "You are Overweight need to lose weight";
 	} else if (bmi >= 30) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
+		measure = "Your BMI is " + bmi + " which means " + "You are Obese need to lose weight";
 	}
 	
 
